98a061a5b4d9a6da75d25ee80fb0c4db71b251d5
Nikolay Galkin <mr.galkin@gmail.com>
2017-08-16 18:32:22 +0300

    Hotfix for student detail

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contact', views.contact, name='contact'),
     url(r'^student_list', views.student_list, name='student_list'),
-    url(r'^student_detail/(?P<pk>[0-9]+)/$', views.student_detail, name='student_detail'),
+    url(r'^student_detail', views.student_detail, name='student_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
