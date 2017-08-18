19576da37f785a824c602735ac8fcc27ba90adaa
Kostyantin Kedrya <kedryakv@gmail.com>
2017-08-18 13:29:25 +0300

    Fix pull request view issue

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -21,8 +21,8 @@ from . import views
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.HomePageView.as_view(), name='index'),
-    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
-    url(r'^student_list/$', views.StudentListView.as_view(), name='student_list'),
-    url(r'^student_detail/$', views.StudentDetailView.as_view(), name='student_detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^student_list/$', views.student_list, name='student_list'),
+    url(r'^student_detail/$', views.student_detail, name='student_detail'),
 ]
