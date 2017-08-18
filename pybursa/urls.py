6167f691d540de4c669736d6ea3837d1d31b5437
Kostyantin Kedrya <kedryakv@gmail.com>
2017-08-18 19:29:34 +0300

    Init quadratic app

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^quadratic/', include('quadratic.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
