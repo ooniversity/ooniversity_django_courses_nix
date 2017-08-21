e5f25dedcb2f307b15574a3daedce00d80f0ef04
ViacheslavKovalenko <kovalenko_v@nixsolutions.com>
2017-08-21 19:25:39 +0300

    added static pybursa files to django structure

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    # static pybursa urls
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^student_list/', views.student_list, name='student_list'),
+    url(r'^student_detail/', views.student_detail, name='student_detail'),
+
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
     url(r'^quadratic/', include('quadratic.urls')),
