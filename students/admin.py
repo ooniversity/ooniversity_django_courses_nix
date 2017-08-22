82e827dbb38bbff79a714991be2a50fb1908a8ea
UbuntuVM <ub@ntu.me>
2017-08-22 11:06:58 +0300

    dj5_2(1)

--- students/admin.py
+++ students/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Student
 # Register your models here.
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ('full_name', 'email', 'skype')
+    list_filter = ['courses']
+    filter_horizontal= ['courses']
+    fieldsets = [
+        ('Personal info', {'fields': ['name', 'surname',
+                                      'date_of_birth']}),
+        ('Contact info', {'fields': ['email', 'phone',
+                                     'address', 'skype']}),
+        (None, {'fields': ['courses']}),
+    ]
+
+    def full_name(self, obj):
+        return obj.name + ' ' + obj.surname
+
+
+admin.site.register(Student, StudentAdmin)
