a23b815826adb34a1ec9fe5958602b0e8211021c
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-08-23 18:04:11 +0300

    Task 5.2 Start

--- courses/admin.py
+++ courses/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Course, Lesson
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_description']
+    search_fields = ['name']
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
