c719ba5e49a29b62e7b14a5f8ca0befa2668d445
Butko <higher@ro.ru>
2017-08-24 18:02:46 +0300

    Move main page template back to 'pybursa' app

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,4 @@ from courses import views as courses_views
 app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/', courses_views.courses, name='courses'),
-    url(r'^$', courses_views.index, name='index')
 ]
