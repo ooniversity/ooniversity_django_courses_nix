647ea929d003aebd46db1c2cc49b6d0529da0446
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-08-25 11:38:03 +0300

    Task 5.2

--- courses/admin.py
+++ courses/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from .models import Course, Lesson
 
+class LessonInline(admin.StackedInline):
+	model = Lesson
+
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'short_description']
     search_fields = ['name']
+    inlines = [LessonInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
 
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
--- students/admin.py
+++ students/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email']
+    list_display = ['full_name', 'email', 'skype']
+    list_filter = ['courses']
+    
+    fieldsets = (
+        ('Personal info', {
+            'fields': ('name', 'surname', 'date_of_birth')
+        }),
+        ('Contact info', {
+            'fields': ('email', 'phone', 'address', 'skype', 'courses')
+        }),
+    )
+
+admin.site.register(Student, StudentAdmin)
