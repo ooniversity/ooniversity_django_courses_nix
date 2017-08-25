36873efca34eafaf1f79de004b319405780fbabc
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-08-25 11:52:33 +0300

    Task 5.2 Change on "TabularInline" for inlines in course item view

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Course, Lesson
 
-class LessonInline(admin.StackedInline):
+class LessonInline(admin.TabularInline):
 	model = Lesson
 
 class CourseAdmin(admin.ModelAdmin):
@@ -10,10 +10,5 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['name']
     inlines = [LessonInline]
 
-class LessonAdmin(admin.ModelAdmin):
-    pass
-
-
-
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Lesson)
