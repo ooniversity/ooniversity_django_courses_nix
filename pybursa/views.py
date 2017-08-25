95d54cf09a316055c86a7ee026bbf8a685afba89
butko <MaksimButko@nixsolutions.com>
2017-08-25 09:46:44 +0300

    Remove 'pybursa' subdirectory from templates

--- pybursa/views.py
+++ pybursa/views.py
@@ -4,8 +4,8 @@ from courses.models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'pybursa/index.html', {'course_list': courses})
+    return render(request, 'index.html', {'course_list': courses})
 
 
 def contact(request):
-    return render(request, 'pybursa/contact.html')
+    return render(request, 'contact.html')
