d13ac3d50ddaedd23bf867697069f0d9a8a3ab7f
Butko <higher@ro.ru>
2017-08-27 00:36:34 +0300

    Make migrate data. Customize Coaches for admin

--- coaches/admin.py
+++ coaches/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from coaches.models import Coach
 
 
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ['user.first_name', 'user.last_name']
+    list_filter = ['staff']
+
 admin.site.register(Coach)
