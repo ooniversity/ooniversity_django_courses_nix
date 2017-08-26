c0b0a23c7371f7b90ffd4cdc67f4f8374444a429
Butko <higher@ro.ru>
2017-08-26 21:53:42 +0300

    Add filter and search for courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'short_description']
+    inlines = [LessonInline]
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
