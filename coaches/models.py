11ef0fbe6fdc15963adb18f468f28a47ac2ee45b
Butko <higher@ro.ru>
2017-08-27 00:16:36 +0300

    Add 'Coach' model, relations, register in admin

--- coaches/models.py
+++ coaches/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Coach(models.Model):
+    user = models.OneToOneField(User)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.user.name
