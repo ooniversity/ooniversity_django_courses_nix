0bb7cf4ce154058076de91fca6b5cbf44a5f95f8
artem tarasov <tema.tarasow@gmail.com>
2017-08-29 00:00:57 +0300

    task 5_2

--- students/admin.py
+++ students/admin.py
@@ -6,5 +6,15 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ["get_name_surname", "email", "skype"]
     search_fields = ['surname', 'email']
     list_filter = ["courses"]
+    admin.site.site_header = 'PyBursa Administration'
+
+    fieldsets = (
+        ('Personal info', {
+            'fields': ('name', 'surname', 'date_of_birth')
+        }),
+        ('Contact info', {
+            'fields': ('email', 'phone', 'address', 'skype', 'courses')
+        }),
+    )
 
 admin.site.register(Student, StudentAdmin)
