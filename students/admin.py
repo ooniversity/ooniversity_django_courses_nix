3fceb8ab9476ae60e10abffc8859587907278140
artem tarasov <tema.tarasow@gmail.com>
2017-08-28 09:26:46 +0300

    custom admin

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["get_name_surname", "email", "skype"]
+    search_fields = ['surname', 'email']
+    list_filter = ["courses"]
+
+admin.site.register(Student, StudentAdmin)
