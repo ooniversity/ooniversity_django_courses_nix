3ef8029a9daf1f28da01c07039bb06d7aa3a95f6
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-08-30 23:52:37 +0300

    Task 8 Add course CRUD

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),
 ]
