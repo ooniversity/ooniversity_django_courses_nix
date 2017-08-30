6e297519fd55c7cf46deda070231973cf81fc2d7
Vadym Voskresenskyi <vadim.voskresenskiy@nixsolutions.com>
2017-08-30 21:17:00 +0300

    Task 7.3

--- students/urls.py
+++ students/urls.py
@@ -6,4 +6,7 @@ app_name = 'students'
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),
 ]
