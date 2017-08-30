e0eaae03deab1d49b05308053ad61550a6864141
UbuntuVM <ub@ntu.me>
2017-08-30 11:53:01 +0300

    dj_7_2(3)

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -2,13 +2,39 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField(label='коэффициент a')
-    b = forms.IntegerField(label='коэффициент b')
-    c = forms.IntegerField(label='коэффициент c')
+    a = forms.CharField(label='коэффициент a', max_length=10)
+    b = forms.CharField(label='коэффициент b', max_length=10)
+    c = forms.CharField(label='коэффициент c', max_length=10)
 
     def clean_a(self):
         a_data = self.cleaned_data['a']
-        if a_data == 0:
+
+        try:
+            a_data = int(a_data)
+        except ValueError:
+            raise forms.ValidationError('коэффициент не целое число')
+
+        if a_data == '0':
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
 
         return a_data
+
+    def clean_b(self):
+        b_data = self.cleaned_data['b']
+
+        try:
+            b_data = int(b_data)
+        except ValueError:
+            raise forms.ValidationError('коэффициент не целое число')
+
+        return b_data
+
+    def clean_c(self):
+        c_data = self.cleaned_data['c']
+
+        try:
+            c_data = int(c_data)
+        except ValueError:
+            raise forms.ValidationError('коэффициент не целое число')
+
+        return c_data
