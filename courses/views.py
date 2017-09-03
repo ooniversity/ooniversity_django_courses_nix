214f4b17cb8bafad9d1561d27e9ad6515aa19169
AlexanderPavlenko94 <alex.pavlenko.94@gmail.com>
2017-09-03 22:26:58 +0300

    Week4 Task2

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import Course
+from .forms import CourseModelForm, LessonModelForm
 
 def detail(request, pk):
     course = None
@@ -12,3 +14,49 @@ def detail(request, pk):
         pass
     
     return render(request, 'courses/detail.html', {'course' : course})
+
+def add(request):
+    if request.method == 'POST':
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            course = form.save()
+            messages.success(request, 'Course ' + course.name + ' has been successfully added.')
+            return redirect('index')
+    else:
+        form = CourseModelForm
+    return render(request, 'courses/add.html', {'form': form})
+
+
+def edit(request, pk):
+    course = Course.objects.get(id=pk)
+    if request.method == 'POST':
+        form = CourseModelForm(request.POST, instance=course)
+        if form.is_valid():
+            course = form.save()
+            messages.success(request, 'The changes have been saved.')
+            return redirect('courses:edit', pk=course.id)
+    else:
+        form = CourseModelForm(instance=course)
+    return render(request, 'courses/edit.html', {'form': form})
+
+
+def remove(request, pk):
+    course = Course.objects.get(id=pk)
+    if request.method == 'POST':
+        course.delete()
+        messages.success(request, 'Course ' + course.name + ' has been deleted.')
+        return redirect('index')
+    return render(request, 'courses/remove.html', {'course': course})
+
+
+def add_lesson(request, pk):
+    course = Course.objects.get(id=pk)
+    if request.method == 'POST':
+        form = LessonModelForm(request.POST, initial={'course': course.id})
+        if form.is_valid():
+            lesson = form.save()
+            messages.success(request, 'Lesson ' + lesson.subject + ' has been successfully added.')
+            return redirect('courses:detail', pk=course.id)
+    else:
+        form = LessonModelForm(initial={'course': course.id})
+    return render(request, 'add_lesson.html', {'form': form})
