e5a0b28166604de2fe543a35b39f866b1cfd3ec7
Kostyantin Kedrya <kedryakv@gmail.com>
2017-09-13 19:12:02 +0300

    Task 3-5

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from students.views import students, student
 
-from . import views
-
+app_name = 'students'
 urlpatterns = [
-    url(r'^results/$', views.results, name='results'),
+    url(r'^$', students, name='students'),
+    url(r'^(?P<student_id>[0-9]+)/$', student, name='student'),
 ]
